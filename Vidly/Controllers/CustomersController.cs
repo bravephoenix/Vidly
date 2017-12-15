ce0773638a70ab8c655572c23ad56be7fee6de04
Sandeep <sandeep.daiya45@gmail.com>
2017-12-15 23:59:42 +0530

    Section-2 Exercise : Displaying list of customers and movies

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            if (customers.Count == 0)
+                return View("NoCustomers");
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var custObj = GetCustomers().SingleOrDefault(m => m.Id == id);
+            if (custObj == null)
+                return HttpNotFound();
+
+            return View(custObj);
+        }
+
+        public IList<Customer> GetCustomers()
+        {
+            IList<Customer> customers = new List<Customer>
+            {
+                new Customer(){ Id=1, Name="Sandeep" },
+                new Customer(){ Id=2, Name="Rohit"}
+            };
+            return customers;
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public IList<Movie> GetMovies()
+        {
+            IList<Movie> movies = new List<Movie>
+            {
+                new Movie(){ Id=1, Name="Incredible Hulk" },
+                new Movie(){ Id=2, Name="Iron Man" }
+            };
+            return movies;
+        }
+    }
+}
