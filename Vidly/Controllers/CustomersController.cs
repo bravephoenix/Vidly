b3fc44bc8871bc8c7b4ecc6a2c489fc35eab3dd8
Sandeep <sandeep.daiya45@gmail.com>
2017-12-16 10:00:17 +0530

    Add data annotations and querying data

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using Vidly.Models;
+
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
             if (customers.Count == 0)
                 return View("NoCustomers");
 
@@ -21,21 +35,11 @@ namespace Vidly.Controllers
 
         public ActionResult Details(int id)
         {
-            var custObj = GetCustomers().SingleOrDefault(m => m.Id == id);
+            var custObj = _context.Customers.SingleOrDefault(m => m.Id == id);
             if (custObj == null)
                 return HttpNotFound();
 
             return View(custObj);
-        }
-
-        public IList<Customer> GetCustomers()
-        {
-            IList<Customer> customers = new List<Customer>
-            {
-                new Customer(){ Id=1, Name="Sandeep" },
-                new Customer(){ Id=2, Name="Rohit"}
-            };
-            return customers;
         }
     }
 }
