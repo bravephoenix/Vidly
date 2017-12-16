a69f2f404c18909113f7ebeef15f6ba3f7a3addd
Sandeep <sandeep.daiya45@gmail.com>
2017-12-17 00:13:22 +0530

    Section 3 Exercise 3: Added genre and changed the movie model

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,26 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
 
-        public IList<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            IList<Movie> movies = new List<Movie>
-            {
-                new Movie(){ Id=1, Name="Incredible Hulk" },
-                new Movie(){ Id=2, Name="Iron Man" }
-            };
-            return movies;
+            var movObj = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movObj != null)
+                return View(movObj);
+
+            return HttpNotFound();
         }
     }
 }
